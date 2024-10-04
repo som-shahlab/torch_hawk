d5f6fec9ac59fe8ca4182a956ce3a81464347131
Ethan Steinberg <ethan.steinberg@gmail.com>
2024-10-04 09:18:26 -0700

    No driver

--- build/linux_setup.sh
+++ build/linux_setup.sh
@@ -4,13 +4,13 @@ chmod +x /usr/local/bin/bazel
 dnf config-manager --set-enabled powertools
 dnf makecache
 
-dnf -y install epel-release
-dnf upgrade
+# dnf -y install epel-release
+# dnf upgrade
 
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
 dnf makecache
 
-dnf module install nvidia-driver:latest-dkms
+#  dnf module install nvidia-driver:latest-dkms
 dnf install cuda-toolkit
 
 export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}
