1e1a9344842e85c6b426ff1e33fda4431aa9955c
Ethan Steinberg <ethan.steinberg@gmail.com>
2024-10-04 09:01:17 -0700

    Better

--- build/linux_setup.sh
+++ build/linux_setup.sh
@@ -1,12 +1,14 @@
 curl -o /usr/local/bin/bazel -L  https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64
 chmod +x /usr/local/bin/bazel
 
-dnf install kernel-devel-$(uname -r) kernel-headers-$(uname -r)
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf config-manager --set-enabled powertools
+dnf makecache
 
-dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-$distro.repo
+dnf -y install epel-release
+dnf upgrade
 
-dnf clean expire-cache
+dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+dnf makecache
 
 dnf module install nvidia-driver:latest-dkms
 dnf install cuda-toolkit
