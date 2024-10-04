2c2d0e6e382762eb44a63f4e3053a0e83bbdec9d
Ethan Steinberg <ethan.steinberg@gmail.com>
2024-10-04 08:48:41 -0700

    Another way

--- build/linux_setup.sh
+++ build/linux_setup.sh
@@ -1,7 +1,15 @@
 curl -o /usr/local/bin/bazel -L  https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64
 chmod +x /usr/local/bin/bazel
 
-curl -o cuda.run https://developer.download.nvidia.com/compute/cuda/12.6.2/local_installers/cuda_12.6.2_560.35.03_linux.run
-sh cuda.run --silent --toolkit
+
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+# error mirrorlist.centos.org doesn't exists anymore.
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+yum install cuda-toolkit-12-6
+
+#ln -s cuda-12.2 /usr/local/cuda
+
 
 nvcc --version
