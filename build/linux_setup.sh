2451db90ba062f2d421a076845661ea030821e46
Ethan Steinberg <ethan.steinberg@gmail.com>
2024-10-04 08:31:06 -0700

    Next

--- build/linux_setup.sh
+++ build/linux_setup.sh
@@ -1,7 +1,7 @@
 curl -o /usr/local/bin/bazel -L  https://github.com/bazelbuild/bazelisk/releases/download/v1.16.0/bazelisk-linux-amd64
 chmod +x /usr/local/bin/bazel
 
-wget https://developer.download.nvidia.com/compute/cuda/12.6.2/local_installers/cuda_12.6.2_560.35.03_linux.run
-sh cuda_12.6.2_560.35.03_linux.run --silent --toolkit
+curl -o cuda.run https://developer.download.nvidia.com/compute/cuda/12.6.2/local_installers/cuda_12.6.2_560.35.03_linux.run
+sh cuda.run --silent --toolkit
 
 nvcc --version
